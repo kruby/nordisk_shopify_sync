eb22dfa50a2fda40845acb06e3f4e84d960198ba
Thomas K. Schmidt <ts@kruby.dk>
2025-07-04 19:53:52 +0000

    2024-07 rettelse

--- update_app.py
+++ update_app.py
@@ -12,7 +12,7 @@ STORE_C_URL = st.secrets["STORE_C_URL"]
 TOKEN = st.secrets["TOKEN_A"]
 TOKEN_B = st.secrets["TOKEN_B"]
 TOKEN_C = st.secrets["TOKEN_C"]
-API_VERSION = "2023-10"
+API_VERSION = "2024-07"
 
 SYNC_NAMESPACE = "sync"
 SYNC_KEY = "sync_fields"
